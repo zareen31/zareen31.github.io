1a3b3591e4221ea1fdc15963acbfad52e2763b42
zareen31 <72728053+zareen31@users.noreply.github.com>
2021-01-05 10:29:42 +0500

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 		 <p>
-          Test
+          My Paragraph
         </p>
         <a
           className="App-link"
